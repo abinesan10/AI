ba99a1ee0f6e053c523c6051c3abe33f70e0817e
abinesan <abinesan1@outlook.com>
2020-11-02 18:55:38 +0530

    changes

--- crackdetection/settings.py
+++ crackdetection/settings.py
@@ -41,7 +41,9 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     'users',
+
 ]
 
 MIDDLEWARE = [
@@ -52,7 +54,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.BrokenLinkEmailsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+CORS_ORIGIN_ALLOW_ALL = True 
+
 
 ROOT_URLCONF = 'crackdetection.urls'
 
