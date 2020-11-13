946b1096c82a3c58639d6359c5a57e3cdc607d40
abinesan <abinesan1@outlook.com>
2020-11-13 12:16:36 +0530

    changes

--- crackdetection/settings.py
+++ crackdetection/settings.py
@@ -84,15 +84,12 @@ WSGI_APPLICATION = 'crackdetection.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-'default': {
-'ENGINE': 'django.db.backends.mysql',
-'NAME': "u496854317_twitter",
-'USER':"u496854317_twitter",
-'PASSWORD':"Twitter1",
-'HOST':"sql172.main-hosting.eu",
-'CONN_MAX_AGE': 3600
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
-}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
